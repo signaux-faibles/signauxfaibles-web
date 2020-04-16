cdcb975631d72fb01f5e2ec06d00a90b06ebdfbc
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2020-04-16 16:13:19 +0200

    suppression accès compte utilisateur

--- src/views/NavigationDrawer.vue
+++ src/views/NavigationDrawer.vue
@@ -56,15 +56,6 @@
 
         <v-divider></v-divider>
 
-        <v-list-tile href="/auth/realms/master/account">
-          <v-list-tile-action>
-            <v-icon>vpn_key</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Profil utilisateur</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile> 
-
         <v-list-tile @click="logout()">
           <v-list-tile-action>
             <v-icon>logout</v-icon>
