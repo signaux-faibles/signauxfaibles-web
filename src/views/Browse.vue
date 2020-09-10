2a36a867c2ae8bc03a616a6cfd96ec2409b61588
chrnin <christophe.ninucci@direccte.gouv.fr>
2020-09-10 09:23:32 +0200

    clean

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -12,50 +12,8 @@
         </v-btn>
       </form>
     </div>
-
     <PredictionWidget v-for="r in result.results" :key="r.siret" :prediction="r" />
   </div>
-    <!-- <div style="margin: 0px 80px;" v-if="((result.etablissement || []).length > 0 && searched)">
-      {{ result.total_results || (result.etablissement || []).length }} {{ 'resultat' + ((result.total_results > 1) ? 's' : '')}}
-      <div style="margin:20px; font-size: 11px" v-for="e of result.etablissement" :key="e.id">
-        <a style="font-size: 22px" @click="siret=e.siren+e.nic; dialog=true;">{{ e.nom_raison_sociale }}</a><br/>
-        {{ e.libelle_nature_juridique_entreprise }} {{ e.siren + e.nic }} ({{ e.nic == e.nic_siege ? 'siège' : 'secondaire' }})<br/>
-        <span>{{ e.libelle_activite_principale }} (APE {{ e.activite_principale }})</span><br/>
-        {{ e.l6_normalisee }}
-        {{ e.l7_normalisee }}
-      </div>
-    </div> -->
-
-    <!-- <div v-if="(result.etablissement || []).length > 0" style="height: 140px; width: 100%; text-align: center; vertical-align: middle; padding: 10px 10%"> 
-      <v-btn 
-        :disabled="page <= 1"
-        @click="page -= 1; lookup()" 
-        icon>
-        <v-icon>arrow_back</v-icon>
-      </v-btn> 
-      page {{ page }}/{{ result.total_pages || 1 }} 
-      <v-btn 
-        :disabled="page >= (result.total_pages || 1)"
-        @click="page +=1; lookup()" 
-        icon>
-        <v-icon>arrow_forward</v-icon>
-      </v-btn>
-    </div>
-    <div class="empty_text" v-if="(result.etablissement || []).length == 0 && searched">
-      Aucun résultat pour cette recherche.
-    </div>
-    <v-dialog  lazy fullscreen v-model="dialog">
-      <div style="height: 100%; width: 100%; font-weight: 800; font-family: 'Oswald', sans;">
-        <v-toolbar fixed class="toolbar" height="35px" style="color: #fff; font-size: 22px;">
-          <v-spacer/>
-            ETABLISSEMENT
-          <v-spacer/>
-          <v-icon @click="dialog=false;" style="color: #fff">mdi-close</v-icon>
-        </v-toolbar>
-        <Etablissement :siret="siret" :batch="currentBatchKey"></Etablissement>
-      </div>
-    </v-dialog>
-  </div> -->
 </template>
 
 <script>
