264d1831b6a631dddab482c770e7e04ed65d1290
fcoufour <github@coufourier.com>
2021-06-09 11:10:22 +0200

    feat: recherche avancée des établissements

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,24 +1,108 @@
 <template>
   <div>
-    <Toolbar title="Consultation" drawer/>
+    <Toolbar title="Consultation" drawer />
     <div :class="((result.etablissement || []).length > 0 || searched) ? 'loaded' : 'empty'">
       <form v-on:submit.prevent="load">
-        <div :class="((result.etablissement || []).length > 0 || searched) ? 'loaded_picto' : 'empty_picto'">
+        <div
+          :class="((result.etablissement || []).length > 0 || searched) ? 'loaded_picto' : 'empty_picto'"
+        >
           <img height="50" src="../assets/text_signaux_faibles.svg" />
         </div>
-        <v-text-field solo placeholder="Raison sociale ou SIRET" v-model="search" :disabled="loading"></v-text-field>
-        <v-layout wrap>
-          <v-flex xs12 md6>
-            <v-subheader>Zone géographique</v-subheader>
-            <v-radio-group v-model="radios" :mandatory="false" class="mt-0" style="font-size: 14px" :disabled="loading">
-              <v-radio label="Parmi tous les établissements de ma zone géographique" value="geo"></v-radio>
-              <v-radio label="Parmi tous les établissements des entreprises implantées dans ma zone géographique" value="visible"></v-radio>
-              <v-radio label="Sur toute la France" value="tout"></v-radio>
-            </v-radio-group>
+        <v-text-field
+          solo
+          placeholder="Raison sociale ou SIRET"
+          v-model="search"
+          :disabled="loading"
+        ></v-text-field>
+        <v-layout wrap style="margin-top: -16px">
+          <v-flex>
+            <v-checkbox v-model="siegeUniquement" class="mt-0" :disabled="loading">
+              <template v-slot:label>
+                <span style="font-size: 14px">N'afficher que les sièges des entreprises</span>
+              </template>
+            </v-checkbox>
           </v-flex>
-          <v-flex xs12 md6>
-            <v-subheader>Siège des entreprises</v-subheader>
-            <v-checkbox v-model="siegeUniquement" label="N'afficher que les sièges des entreprises" class="mt-0" :disabled="loading"></v-checkbox>
+          <v-flex shrink>
+            <a @click="avance=!avance">Recherche {{ avance ? 'simple' : 'avancée' }}</a>
+          </v-flex>
+        </v-layout>
+        <v-layout wrap v-if="avance">
+          <v-flex xs12 md4 pa-2>
+            <v-subheader>Secteur d'activité</v-subheader>
+            <div
+              style="color: #f00; font-weight: 600;"
+              v-if="currentNaf.length == 0"
+            >Aucun secteur sélectionné</div>
+            <ul style="font-size: 11px; text-align: left" v-if="!allNaf">
+              <li v-for="l in currentNafLibelle.slice(0,4)" :key="l">{{ l }}</li>
+            </ul>
+            <div
+              style="margin-left: 15px; font-size: 11px; color: #444; text-align: left"
+              v-if="currentNaf.length > 4 && !allNaf"
+            >+ {{ currentNaf.length - 4 }} autre{{ (currentNaf.length > 5)?'s':'' }}</div>
+            <div
+              style="margin-left: 15px; font-size: 11px; color: #444"
+              v-if="allNaf"
+            >Tout secteur confondu</div>
+            <v-dialog v-model="nafDialog" persistent scrollable width="700">
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  light
+                  color="rgba(0,0,0,0.74)"
+                  :disabled="loading"
+                  v-on="on"
+                  @click="copyNaf()"
+                  outline
+                  class="ma-3"
+                >
+                  <v-icon>mdi-filter</v-icon>selection des secteurs
+                </v-btn>
+              </template>
+              <v-card>
+                <v-toolbar dark dense color="indigo">
+                  <v-toolbar-title>Sélectionner les secteurs d'activité</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-list>
+                    <v-list-tile>
+                      <v-list-tile-action>
+                        <v-icon
+                          style="cursor: pointer"
+                          @click="selectAllNaf()"
+                        >{{ allNextNaf ? 'mdi-close-box' : someNextNaf ? 'mdi-minus-box' : 'mdi-checkbox-blank-outline' }}</v-icon>
+                      </v-list-tile-action>
+                      {{ !allNextNaf ? 'Tout sélectionner' : 'Tout désélectionner' }}
+                      <v-list-tile-content></v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider />
+                    <v-list-tile v-for="n in naf1" :key="n.value">
+                      <v-list-tile-action @click="toggleNaf(n.value)">
+                        <v-icon style="cursor: pointer;">
+                          {{ nextNaf.includes(n.value) ?
+                          'mdi-checkbox-marked' :
+                          'mdi-checkbox-blank-outline'
+                          }}
+                        </v-icon>
+                      </v-list-tile-action>
+                      <v-list-tile-content>{{ n.text }}</v-list-tile-content>
+                    </v-list-tile>
+                  </v-list>
+                </v-card-text>
+                <v-card-actions style>
+                  <v-spacer />
+                  <v-btn light color="error" @click="nafDialog=false">annuler</v-btn>
+                  <v-btn light color="success" @click="applyNaf(); nafDialog=false">appliquer</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-flex>
+          <v-flex xs12 md4 pa-2>
+            <v-subheader>Zone géographique</v-subheader>
+            <v-select :items="subzones" v-model="zone" return-object></v-select>
+          </v-flex>
+          <v-flex xs12 md4 pa-2>
+            <v-subheader>Effectif minimum</v-subheader>
+            <v-combobox :items="effectifClass" v-model="effectifMin"></v-combobox>
           </v-flex>
         </v-layout>
         <v-btn type="submit" style="width: 150px" :disabled="loading || search.length < 3">
@@ -32,158 +116,250 @@
 </template>
 
 <script>
-  import Toolbar from '@/components/Toolbar.vue'
-  import PredictionWidget from '@/components/PredictionWidget.vue'
-  import Spinner from '@/components/Spinner.vue'
+import Toolbar from '@/components/Toolbar.vue'
+import PredictionWidget from '@/components/PredictionWidget.vue'
 
-  export default {
-    data() {
-      return {
-        search: '',
-        searched: false,
-        result: [],
-        total: 0,
-        page: 0,
-        ignorezone: true,
-        ignoreroles: true,
-        radios: 'geo',
-        listHeight: 0,
-        complete: false,
-        loading: false,
-        errorOccured: false,
-        siegeUniquement: false,
+export default {
+  name: 'Browse',
+  components: { PredictionWidget, Toolbar },
+  data() {
+    return {
+      search: '',
+      searched: false,
+      result: [],
+      total: 0,
+      page: 0,
+      ignorezone: true,
+      ignoreroles: true,
+      radios: 'geo',
+      listHeight: 0,
+      complete: false,
+      loading: false,
+      errorOccured: false,
+      siegeUniquement: false,
+      nafDialog: false,
+      nextNaf: [],
+      effectifClass: [0, 10, 20, 50, 100],
+      avance: false,
+      currentNaf: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+      zone: {
+        text: 'Ma zone géographique',
+        value: 'Ma zone géographique',
+      },
+      effectifMin: 10,
+    }
+  },
+  methods: {
+    load() {
+      this.page = 0
+      this.complete = false
+      this.searched = true
+      this.result = []
+      let eventName = this.search
+      if (this.zone.text === 'Ma zone géographique') {
+        eventName = eventName.concat(',etablissements_zone')
+      } else if (this.zone.text === 'Toute zone') {
+        eventName = eventName.concat(',france')
+      } else {
+        eventName = eventName.concat(',' + this.zone.text)
+      }
+      this.trackMatomoEvent('consultation', 'rechercher', eventName)
+      this.lookupPage()
+    },
+    lookupPage() {
+      this.loading = true
+      this.errorOccured = false
+      this.$axios.post(this.searchURL, this.params).then((response) => {
+        if (response.status === 200) {
+          this.result = this.result.concat(response.data.results)
+          this.total = response.data.total
+          this.trackMatomoEvent('consultation', 'resultats_recherche', this.search, this.total)
+          const p = response.data.page ? response.data.page : 0
+          const pageMax = response.data.pageMax ? response.data.pageMax : 0
+          if (p === pageMax) {
+            this.complete = true
+          }
+          this.page += 1
+        } else if (response.status === 204) {
+          if (this.page === 0) {
+            this.result = []
+            this.total = 0
+          }
+          this.complete = true
+        } else {
+          this.errorOccured = true
+        }
+      }).catch((error) => {
+        this.errorOccured = true
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    applyNaf() {
+      this.currentNaf = this.nextNaf
+    },
+    copyNaf() {
+      this.nextNaf = this.currentNaf.map((n) => n)
+    },
+    toggleNaf(value) {
+      if (this.nextNaf.includes(value)) {
+        this.nextNaf = this.nextNaf.filter((n) => (n !== value))
+      } else {
+        this.nextNaf.push(value)
       }
     },
-    methods: {
-      load() {
-        this.page = 0
-        this.complete = false
-        this.searched = true
-        this.result = []
-        let eventName = this.search
-        if (this.radios === 'visible') {
-          eventName = eventName.concat(',entreprises_zone')
-        } else if (this.radios === 'tout') {
-          eventName = eventName.concat(',france')
-        } else {
-          eventName = eventName.concat(',etablissements_zone')
-        }
-        this.trackMatomoEvent('consultation', 'rechercher', eventName)
+    selectAllNaf() {
+      if (!this.allNextNaf) {
+        this.nextNaf = this.naf1.map((n) => n.value)
+      } else {
+        this.nextNaf = []
+      }
+    },
+  },
+  watch: {
+    scrollTop() {
+      this.listHeight = this.$el.getBoundingClientRect().bottom
+    },
+    result() {
+      this.listHeight = this.$el.getBoundingClientRect().bottom
+    },
+    resultIsEnough() {
+      if (!this.resultIsEnough) {
+        this.trackMatomoEvent('consultation', 'voir_page_suivante', this.search, this.page)
         this.lookupPage()
-      },
-      lookupPage() {
-        this.loading = true
-        this.errorOccured = false
-        this.$axios.get(this.searchURL, {params: this.params}).then((response) => {
-          if (response.status === 200) {
-            this.result = this.result.concat(response.data.results)
-            this.total = response.data.total
-            this.trackMatomoEvent('consultation', 'resultats_recherche', this.search, this.total)
-            this.page += 1
-          } else if (response.status === 204) {
-            if (this.page === 0) {
-              this.result = []
-              this.total = 0
-            }
-            this.complete = true
-          } else {
-            this.errorOccured = true
-          }
-        }).catch((error) => {
-          this.errorOccured = true
-        }).finally(() => {
-          this.loading = false
-        })
-      },
+      }
     },
-    watch: {
-      scrollTop() {
-        this.listHeight = this.$el.getBoundingClientRect().bottom
-      },
-      result() {
-        this.listHeight = this.$el.getBoundingClientRect().bottom
-      },
-      resultIsEnough() {
-        if (!this.resultIsEnough) {
-          this.trackMatomoEvent('consultation', 'voir_page_suivante', this.search, this.page)
-          this.lookupPage()
-        }
-      },
+  },
+  computed: {
+    resultIsEnough() {
+      return !this.searched || this.complete || this.loading || this.height * 2 < this.listHeight || this.errorOccured
     },
-    computed: {
-      resultIsEnough() {
-        return !this.searched || this.complete || this.loading || this.height * 2 < this.listHeight || this.errorOccured
-      },
-      searchURL() {
-        return `/etablissement/search/${this.search}`
-      },
-      params() {
-        const p = {
-          page: this.page,
+    searchURL() {
+      return `/etablissement/search`
+    },
+    params() {
+      const p = {
+        search: this.search,
+        page: this.page,
+      }
+      p.activites = this.currentNaf
+      if (this.zone.text === 'Ma zone géographique') {
+        p.ignorezone = false
+        p.ignoreroles = false
+      } else if (this.zone.text === 'Toute zone') {
+        p.ignorezone = true
+        p.ignoreroles = true
+      } else {
+        p.ignorezone = true
+        p.ignoreroles = true
+        if (this.zone.value.length > 0) {
+          p.departements = this.zone.value
         }
-        if (this.radios === 'visible' || this.radios === 'tout') {
-          p.ignorezone = true
-        }
-        if (this.radios === 'tout') {
-          p.ignoreroles = true
-        }
-        if (this.siegeUniquement) {
-          p.siegeUniquement = this.siegeUniquement
-        }
-        return p
-      },
-      scrollTop() {
-        return this.$store.state.scrollTop
-      },
-      height: {
-        get() {
-          return this.$store.state.height
+      }
+      if (this.effectifMin > 0) {
+        p.effectifMin = parseInt(this.effectifMin, 10)
+      }
+      if (this.siegeUniquement) {
+        p.siegeUniquement = this.siegeUniquement
+      }
+      return p
+    },
+    subzones() {
+      const divider = {
+        divider: true,
+      }
+      let all = [
+        {
+          text: 'Toute zone',
+          value: 'Toute zone',
         },
-        set(height) {
-          this.$store.dispatch('setHeight', height)
+        divider,
+        {
+          text: 'Ma zone géographique',
+          value: 'Ma zone géographique',
         },
-      },
-    },
-    filters: {
-      pluralizeResultats(count) {
-        if (count === 0) {
-          return 'Pas de résultat'
-        } else if (count === 1) {
-          return '1 résultat'
-        } else {
-          return count + ' résultats'
+        {...divider},
+      ]
+      const region = Object.keys(this.$store.state.region).map((r) => {
+        return {
+          text: r,
+          value: this.$store.state.region[r],
         }
-      },
+      }).sort((r1, r2) => r1.text > r2.text)
+      all = all.concat(region)
+      all.push({...divider})
+      const departement = Object.keys(this.$store.state.departements).map((d) => {
+        return {
+          text: d + ' ' + this.$store.state.departements[d],
+          value: [d],
+        }
+      }).sort((d1, d2) => d1.value[0] > d2.value[0])
+      all = all.concat(departement)
+      return all
     },
-    components: { PredictionWidget, Toolbar },
-  }
+    currentNafLibelle() {
+      return this.currentNaf.map((n) => {
+        return this.naf1.filter((n1) => (n1.value === n))[0].text
+      })
+    },
+    naf1() {
+      return Object.keys(this.$store.state.naf).map((n) => {
+        return {
+          text: n + '\u00a0-\u00a0' + this.$store.state.naf[n],
+          value: n,
+        }
+      })
+    },
+    allNaf() {
+      return this.currentNaf.length === this.naf1.length
+    },
+    allNextNaf() {
+      return this.nextNaf.length === this.naf1.length
+    },
+    someNextNaf() {
+      return this.nextNaf.length > 0 && !this.allNextNaf
+    },
+  },
+  filters: {
+    pluralizeResultats(count) {
+      if (count === 0) {
+        return 'Pas de résultat'
+      } else if (count === 1) {
+        return '1 résultat'
+      } else {
+        return count + ' résultats'
+      }
+    },
+  },
+}
 </script>
 
 <style scoped>
-  .empty {
-    height: 150px;
-    width: 100%; 
-    text-align: center; 
-    vertical-align: middle; 
-    padding: 10px 30%;
-  }
-  .empty_picto {
-    margin-top: 20%;
-    font-size: 40px;
-    margin-bottom: 40px;
-  }
-  .loaded_picto {
-    visibility: hidden;
-  }
-  .loaded {
-    width: 100%;
-    text-align: center;
-    vertical-align: middle;
-    padding: 10px 10%;
-  }
-  div.numbers {
-    font-size: 25px;
-    width: 100%;
-    text-align: center;
-  }
+.empty {
+  height: 150px;
+  width: 100%;
+  text-align: center;
+  vertical-align: middle;
+  padding: 10px 10%;
+}
+.empty_picto {
+  margin-top: 20%;
+  font-size: 40px;
+  margin-bottom: 40px;
+}
+.loaded_picto {
+  visibility: hidden;
+}
+.loaded {
+  width: 100%;
+  text-align: center;
+  vertical-align: middle;
+  padding: 10px 10%;
+}
+div.numbers {
+  font-size: 25px;
+  width: 100%;
+  text-align: center;
+}
 </style>
