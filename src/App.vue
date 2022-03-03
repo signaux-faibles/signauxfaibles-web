7e4c93f7c2adf367df24022f0f05d192b5f6c5ef
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2022-03-03 16:22:47 +0100

    fix spacing on buttons Follow.vue

--- src/App.vue
+++ src/App.vue
@@ -97,8 +97,6 @@ export default {
 }
 </script>
 <style>
-/* @import url("https://fonts.googleapis.com/css?family=Abel"); */
-/* @import url("https://fonts.googleapis.com/css?family=Oswald"); */
 
 @font-face {
   font-family: "Roboto";
