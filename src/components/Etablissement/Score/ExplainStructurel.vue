5990a1594d1a15f864a520908b5bb772106b955d
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2022-12-01 19:43:21 +0100

    améliorations UI + incorportation du gitbook

--- src/components/Etablissement/Score/ExplainStructurel.vue
+++ src/components/Etablissement/Score/ExplainStructurel.vue
@@ -1,26 +1,9 @@
 <template>
   <div v-if="permScore && dernierScore && dernierScore.macroRadar && Object.keys(dernierScore.macroRadar).length > 2 && hasAlert">
     <v-toolbar dense flat class="mytoolbar" light>
-      <v-toolbar-title class="localtoolbar mytoolbar">Analyse pré-crise (mars 2020)</v-toolbar-title>
-      <v-spacer />
-      <Help titre="Analyse pré-crise">
-        <template>
-          L'analyse pré-crise utilise des techniques de machine learning pour établir une prédiction de faillite à 18 mois 
-          à l'entrée de la crise COVID, sur les bases du comportement historique des entreprises, au dernier moment où les données 
-          n'étaient pas encore perturbées par les mesures gouvernementales<br/>
-          Les variables apportées à cet algorithme comportent des informations sur:
-          <ul>
-            <li>Données financières</li>
-            <li>Dettes fiscales et sociales</li>
-            <li>Retards de paiement des fournisseurs, lorsqu'ils sont disponibles</li>
-            <li>Demandes et consommation d'activité partielle</li>
-          </ul>
-          Le score ainsi obtenu est présenté, lorsque c'est possible, sous forme d'un radar détaillant le poids de chaque famille de variable dans le score total.
-          Ce score sert ensuite de base pour les redressements experts. Si aucun redressement expert ne s'applique alors le score est présenté sous forme d'alerte.
-        </template>
-      </Help>
+      <v-toolbar-title class="localtoolbar mytoolbar">Analyse pré-crise (prédiction statistique à mars 2020)</v-toolbar-title>
     </v-toolbar>
-    <v-card outlined>
+    <v-card v-if="typeExplication==='mixte' || typeExplication==='structurel'"outlined>
       <v-card-text>
         <div>
           Score pré-crise:
@@ -32,10 +15,21 @@
           ></apexchart>
         </div>
         <div v-if="selectConcerning(dernierScore)">
-          Les variables les plus significatives sont, par ordre d'importance:
+          Les variables les plus significatives sont, par ordre d'importance :
           <ul >
             <li v-for="c in selectConcerning(dernierScore)" :key="c[1]"><em>{{ libelleMicro(c[1]) }}</em> ({{ libelleMacro(c[0]) }})</li>
           </ul>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card v-else>
+      <v-card-text>
+        La détection ne se base pas sur des éléments historiques pré-crise, le radar n'est donc pas disponible.<br/>
+        <div style="text-align: center">
+          <p/>
+          <v-icon size="120px" color="grey lighten-2">
+            mdi-compass-off-outline
+          </v-icon>
         </div>
       </v-card-text>
     </v-card>
@@ -52,7 +46,7 @@ export default {
       variablesMacro: ['activite_partielle', 'dette_urssaf', 'retards_paiement', 'sante_financiere'],
     }
   },
-  props: ['historique', 'summary'],
+  props: ['historique', 'summary', 'typeExplication'],
   components: {Help},
   methods: {
     series(h) {
@@ -151,21 +145,6 @@ export default {
     },
     hasAlert() {
       return (this.alert === "Alerte seuil F1" || this.alert === "Alerte seuil F2")
-    },
-    typeExplication() {
-      if (this.crash) {
-        return "crash" 
-      } else if (this.alert == null) {
-        return "horsperimetre"
-      } else if (this.hasRedressements && this.hasAlert) {
-        return "mixte"
-      } else if (this.hasRedressements && !this.hasAlert) {
-        return "conjoncturel"
-      } else if (!this.hasRedressements && this.hasAlert) {
-        return "structurel" 
-      } else {
-        return "ras"
-      }
     },
   }
 }
---
+++ src/views/news/News003.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header ripple>
+      <h1>Publication de signalements — Décembre 2022</h1>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-card flat>
+        <v-card-text class="text">
+          Signaux Faibles consolide continuellement la production de ses signalements, voici quelques informations pour suivre les dernières évolutions :
+          <h2>Ajout de critères experts financiers</h2>
+          <ul>
+            <li>Les 3 critères experts de la liste précédente ont été maintenus et actualisés avec les dernières données disponibles.</li>
+            <li>3 nouveaux critères experts portant sur la santé financière de l'entreprise ont été introduits</li>
+          </ul>
+          <Gitbook target="f.a.q.#comment-sont-obtenues-les-listes-comment-fonctionne-le-modele-algorithmique"/>
+          <p/>
+          <h2>Statistiques de la nouvelle liste</h2>
+          <ul>
+            <li>nombre total de signalements : 31174</li>
+            <li>nombre de premières alertes : 15227</li>
+            <ul>
+            <li>dont 12164 sont signalées uniquement grâce aux nouveaux critères financiers</li>
+            </ul>
+          </ul>
+          <p/>
+          Ce dernier point explique notamment la hausse de 3124% des premières alertes par rapport à la publication de septembre 2022.
+          <p/>
+
+          <h2>Mise à jour des données</h2>
+          Les données ont été actualisées en octobre 2022 et mises en ligne dans l'applicatif. <br/>
+          Voici leur état de fraicheur :
+          <ul>
+            <li>Données fiscales (DGFiP) : données à août 2022, non consultables directement dans l'applicatif.</li>
+            <li>Dettes, cotisations, procédures collectives (URSSAF) : données à septembre 2022.</li>
+            <li>Activité partielle (DGEFP) : données à octobre 2022.</li>
+            <li>Comportement de paiement (Paydex) : données à octobre 2022.</li>
+            <li>Référentiel des entreprises (Sirene) : données à octobre 2022.</li>
+            <li>Données financières publiques : données jusqu'à l'exercice 2019.</li>
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script lang="js">
+import Gitbook from '@/components/Gitbook.vue'
+
+export default {
+  components: {Gitbook},
+  props: ['activePanel']
+}
+</script>
+
+<style scoped>
+.text {
+  line-height: 1.7;
+}
+.red-darken-4 {
+  color: #B71C1C;
+}
+h1 {
+  color: #333;
+}
+</style>
