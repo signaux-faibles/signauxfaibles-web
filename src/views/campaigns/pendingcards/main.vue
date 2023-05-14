d9806a0fe84ff810c0ae804de4060eaa81b2c5e2
Christophe Ninucci <christophe@zbouboutchi.net>
2023-05-14 07:27:25 +0200

    boutons

--- src/views/campaigns/pendingcards/main.vue
+++ src/views/campaigns/pendingcards/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Card v-for="card in pendingCards" @key="card.siret">
+    <Card v-for="card in pendingCards" :key="card.siret">
         <template v-slot:entete>
           <div>
             <ScoreWidget :prediction="prediction(card.siret)" :tooltip="true"/>
@@ -15,8 +15,31 @@
                     {{ card.siret }}
                 </span>
             </div>
+            <div style="text-align: right" class="mr-2">
+                <v-btn color="indigo" dark block small style="text-transform: none" class="mb-2" @click="showEtablissement(card.siret)">Fiche Établissement</v-btn>
+                <v-btn color="indigo" dark block small  style="text-transform: none" @click="showEntreprise(card.siret)">Fiche Entreprise</v-btn>
+            </div>
         </template>
     </Card>
+    <v-dialog fullscreen v-model="dialogEtablissement">
+        <div style="height: 100%; width: 100%; font-weight: 800; font-family: 'Oswald', sans;">
+            <v-toolbar fixed class="toolbar" height="35px" style="color: #fff; font-size: 22px; z-index: 50;">
+                <v-spacer/>FICHE ETABLISSEMENT<v-spacer/>
+                <v-icon @click="hideEtablissement()" style="color: #fff">mdi-close</v-icon>
+            </v-toolbar>
+            <Etablissement v-if="dialogEtablissement" :siret="this.siret"></Etablissement>
+        </div>
+    </v-dialog>
+    <v-dialog fullscreen v-model="dialogEntreprise">
+        <div style="height: 100%; width: 100%; font-weight: 800; font-family: 'Oswald', sans;">
+            <v-toolbar fixed class="toolbar" height="35px" style="color: #fff; font-size: 22px; z-index: 50;">
+                <v-spacer />FICHE ENTREPRISE
+                <v-spacer />
+                <v-icon @click="hideEntreprise()" style="color: #fff">mdi-close</v-icon>
+            </v-toolbar>
+            <Entreprise v-if="dialogEntreprise" :siren="this.siren"/>
+        </div>
+    </v-dialog>
   </div>
 </template>
 
--- src/views/campaigns/pendingcards/script.js
+++ src/views/campaigns/pendingcards/script.js
@@ -1,11 +1,36 @@
 import Card from "@/components/card/main"
 import ScoreWidget from "@/components/ScoreWidget.vue"
-
+import Etablissement from "@/components/Etablissement/Main.vue"
+import Entreprise from "@/components/Entreprise.vue"
 export default {
   name: "CampaignsPendingCards",
   props: ['cards'],
-  components: {Card, ScoreWidget},
+  components: {Card, ScoreWidget, Etablissement, Entreprise},
+  data() {
+    return {
+      dialogEtablissement: false,
+      dialogEntreprise: false,
+      siret: null,
+      siren: null,
+    }
+  },
   methods: {
+    showEtablissement(siret) {
+      this.siret = siret
+      this.dialogEtablissement = true
+    },
+    hideEtablissement() {
+      this.siret = null
+      this.dialogEtablissement = false
+    },
+    showEntreprise(siret) {
+      this.siren = siret.slice(0,9)
+      this.dialogEntreprise = true
+    },
+    hideEntreprise() {
+      this.siret = null
+      this.dialogEntreprise = false
+    },
     prediction(siret) {
       return {
         "alert": "Alerte seuil F" + (siret%2 + 1),
