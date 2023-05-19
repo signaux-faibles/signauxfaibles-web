06c4704949c82f1dc366e9eb4c84823f39c65324
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-05-19 10:49:05 +0200

    confidentialité redressements

--- src/components/Etablissement/Score/Explain.vue
+++ src/components/Etablissement/Score/Explain.vue
@@ -15,6 +15,7 @@
             :signalDiminutionUrssaf="signalDiminutionUrssaf"
             :signalActivitePartielle="signalActivitePartielle"
             :signalFinancier="signalFinancier"
+            :signalConfidentiel="signalConfidentiel"
             :redressements="redressements"
             :typeExplication="typeExplication"
         />
@@ -168,6 +169,7 @@ export default {
         this.signalDiminutionUrssaf,
         this.signalActivitePartielle,
         this.signalFinancier,
+        this.signalConfidentiel,
       ]
     },
     signalAugmentationUrssaf() {
@@ -187,6 +189,9 @@ export default {
       (this.hasRedressement("k_propres_négatifs")?1:0) +
       (this.hasRedressement("rentabilité_faible")?1:0) +
       (this.hasRedressement("tva_rar_elevé")?1:0)) > 1
+    },
+    signalConfidentiel() {
+      return this.hasRedressement("confidentiel")
     },
     typeExplication() {
       if (this.crash) {
