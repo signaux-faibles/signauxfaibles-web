036d7863a723715200c8155ce9b61731c43fefba
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-06-02 07:30:13 +0200

    clean comments

--- src/components/Gitbook.vue
+++ src/components/Gitbook.vue
@@ -11,12 +11,6 @@
         :href="gitbookPath('ROOT') + target">
       <v-icon>fa-book</v-icon><span v-if="!icon"> {{ caption || "En savoir plus" }}</span>
     </v-btn>
-<!--    <v-btn v-if="icon"-->
-<!--           icon-->
-<!--           target="_blank"-->
-<!--           :href="gitbookPath('ROOT') + target">-->
-<!--      <v-icon color="white">fa-book</v-icon>-->
-<!--    </v-btn>-->
   </div>
 </template>
 
