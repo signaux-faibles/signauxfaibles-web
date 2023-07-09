0820c6b224ff00d73103b1ac34f0b03f02603ed5
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-07-09 09:36:50 +0200

    - peupler les écrans quand les résultats sont vides.

--- src/components/campaigns/myactions/main.vue
+++ src/components/campaigns/myactions/main.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div id="empty" v-if="myActions.etablissements.length == 0 && !loading">
-      Vous n'avez aucune prise de contact en cours dans cette campagne.
+      <v-icon size="70px" color="grey" class="mb-10">fa-snowman</v-icon><br/>
+      Vous n'avez aucune prise de contact à réaliser.<br/>
+      <div v-if="campaigns.campaign.nbPending > 0">
+        Vous pouvez en ajouter en cliquant sur <span class="emph">Je prends contact</span> dans l'onglet <span class="emph">À contacter</span>.
+      </div>
     </div>
 
     <Spinner style="min-height: 60vh" v-if="loading"/>
--- src/components/campaigns/pendingActions/main.vue
+++ src/components/campaigns/pendingActions/main.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="min-heigh: 100%">
     <div id="empty" v-if="pending.etablissements.length == 0 && !loading">
-      Il n'y a pas d'entreprise en attente dans cette campagne de prise de contact.
+      Il n'y a aucune entreprise en attente de prise de contact.
     </div>
     <Spinner style="min-height: 60vh"  v-if="loading"/>
     <CampaignsPendingActionsEtablissement v-for="etablissement in etablissements()" :key="etablissement.siret" :etablissement="etablissement"/>
