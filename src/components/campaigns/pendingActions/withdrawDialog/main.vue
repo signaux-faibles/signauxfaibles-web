d41967e8d3c986cdb966e09a2a6832f7cb3e7216
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-09-27 14:13:45 +0200

    ajustement fenêtre

--- src/components/campaigns/pendingActions/withdrawDialog/main.vue
+++ src/components/campaigns/pendingActions/withdrawDialog/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-dialog persistent v-model="withdrawDialog.visible" height=50% width=30%>
+    <v-dialog persistent v-model="withdrawDialog.visible" height=50% width=40%>
       <div style="width: 100%; font-size: 17px; font-weight: 800; font-family: 'Oswald', sans;">
         <v-toolbar color="red darken-3" dark>
           <v-toolbar-title class="localtoolbar">Je repousse la prise de contact de {{ withdrawDialog.raisonSociale }}</v-toolbar-title>
@@ -12,6 +12,15 @@
             <v-radio-group v-model="withdrawDialog.radio">
               <v-radio v-for="(label, value) in withdrawLabels" :key="value" :label="label" :value="value"/>
             </v-radio-group>
+            <v-alert
+              class="ml-4 mr-4 mt-0"
+              dense
+              colored-border
+              type="info"
+
+            >
+              Dans l'immédiat, cette prise de contact sera déplacée en pied de liste.
+            </V-ALERT>
             <div style="text-align: right">
               <v-btn class="mr-4" color="indigo" outlined dark @click="withdrawDialog.hide()">
                 RETOUR
@@ -22,7 +31,6 @@
               </v-btn>
             </div>
           </v-card-text>
-
         </v-card>
       </div>
     </v-dialog>
