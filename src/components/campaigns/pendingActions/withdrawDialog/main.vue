1e27e7ae2376dbbef0a428c618398b0f65da9876
Christophe Ninucci <christophe@zbouboutchi.net>
2023-09-27 06:40:58 +0200

    ajustement phrase withdraw

--- src/components/campaigns/pendingActions/withdrawDialog/main.vue
+++ src/components/campaigns/pendingActions/withdrawDialog/main.vue
@@ -1,27 +1,17 @@
 <template>
   <div>
-    <v-dialog persistent v-model="withdrawDialog.visible" height=50% width=50%>
+    <v-dialog persistent v-model="withdrawDialog.visible" height=50% width=30%>
       <div style="width: 100%; font-size: 17px; font-weight: 800; font-family: 'Oswald', sans;">
         <v-toolbar color="red darken-3" dark>
-          <v-toolbar-title class="localtoolbar">Je repousse cette prise de contact ({{ withdrawDialog.raisonSociale }})</v-toolbar-title>
+          <v-toolbar-title class="localtoolbar">Je repousse la prise de contact de {{ withdrawDialog.raisonSociale }}</v-toolbar-title>
         </v-toolbar>
-        <v-card class="pl-2 pr-2 pb-2">
+        <v-card class="pl-2 pr-2 pb-5">
           <v-card-text class="pb-0 mb-0" style="font-size: 17px">
-            Cet établissement sera automatiquement écarté pour les campagnes à venir. Dans combien de temps souhaitez vous l'ajouter de nouveau dans votre liste des établissements à contacter ?
           </v-card-text>
-          <v-card-text class="pt-0 mt-0">
+          <v-card-text class="pb-0 mb-0 pt-0 mt-0">
             <v-radio-group v-model="withdrawDialog.radio">
               <v-radio v-for="(label, value) in withdrawLabels" :key="value" :label="label" :value="value"/>
             </v-radio-group>
-            <v-alert
-              class="ml-4 mr-4 mt-0"
-              dense
-              colored-border
-              type="info"
-
-            >
-              Dans l'immédiat, cette prise de contact sera déplacée en pied de liste.
-            </v-alert>
             <div style="text-align: right">
               <v-btn class="mr-4" color="indigo" outlined dark @click="withdrawDialog.hide()">
                 RETOUR
