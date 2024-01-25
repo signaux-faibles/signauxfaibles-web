b29387af910f50705bd900fe897cb9e55d176d37
Christophe Ninucci <christophe@zbouboutchi.net>
2024-01-25 14:02:15 +0100

    - ajout du bouton de téléchargement

--- src/components/campaigns/header/script.js
+++ src/components/campaigns/header/script.js
@@ -3,6 +3,32 @@ import StatusLabels from "@/assets/campaignStatus.json";
 export default {
   name: 'CampaignsHeader',
   props: ['campaign'],
+  methods: {
+    // downloadExport(campaignID) {
+    //   this.$axios.get().then((r) => {
+    //     console.log(r.data)
+    //   })
+    // },
+    downloadExport(campaignID) {
+      this.$axios(
+          {
+            method: 'get',
+            url: `/campaign/export/${campaignID}`,
+            responseType: 'arraybuffer',
+          },
+      ).then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const element = document.createElement('a')
+        element.setAttribute('href', url)
+        const filename = response.headers['content-disposition'].split('=')[1]
+        element.setAttribute('download', filename)
+        element.style.display = 'none'
+        document.body.appendChild(element)
+        element.click()
+        document.body.removeChild(element)
+      })
+    },
+  },
   computed: {
     dateFin() {
       return new Date(this.campaign.dateFin)
