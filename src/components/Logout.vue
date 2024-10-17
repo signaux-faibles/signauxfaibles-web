e0ca12aa5796dc726ece07acbdabbc184808f0dc
Charles Marcoin <charles.marcoin@gmail.com>
2024-10-17 16:02:46 +0200

    added an endpoint to logout from NP1

---
+++ src/components/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p>Déconnexion en cours...</p>
+    </div>
+</template>
+  
+<script>
+  export default {
+    created() {
+      this.logout()
+    },
+    methods: {
+        logout() {
+            if (this.$route.path !== '/') {
+                this.$router.push('/')
+            } 
+            this.$keycloak.logoutFn()
+        },
+    }
+  }
+</script>
