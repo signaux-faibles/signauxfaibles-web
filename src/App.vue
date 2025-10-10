754e4e0e2da0e241047136a6e233b47ef4ba4b6e
Charles Marcoin <charles.marcoin@gmail.com>
2025-10-10 10:56:22 +0200

    new banner for trainings

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,15 @@
          </span>
         </v-banner-text>
       </v-banner>
+      <v-banner lines="three" color="blue lighten-5" :stacked="false">
+        <v-banner-text>
+          <span>
+            > Inscrivez-vous aux formations en contactant esther.spindler@beta.gouv.fr ! </br>
+            Formations générales les 15/10 et 10/11 à 15h </br>
+            Formations au module « Accompagnements » : 21/10 à 15h. 29/10, 05/11, 10/11 et 19/11 à 11h
+          </span>
+        </v-banner-text>
+      </v-banner>
       <NavigationDrawer v-if="login && drawers.left"/>
       <router-view/>
     </v-main>
